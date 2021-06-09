c97d1f2fb1df2153424d2130567ae0c9846bb5e4
Benjamin Erhart <berhart@netzarchitekten.com>
2021-06-09 13:37:14 +0200

    Adapted Rust copies of C enums and structs to what's used in updated LwIP.

--- lwip/src/pbuf.rs
+++ lwip/src/pbuf.rs
@@ -3,25 +3,102 @@ use crate::error::err_t;
 use std::io;
 use std::os::raw::c_void;
 
+
+// Consts and enums copied from lwip/src/include/lwip/pbuf.h.
+
+/// Indicates that the payload directly follows the struct pbuf.
+/// This makes @ref pbuf_header work in both directions.
+const PBUF_TYPE_FLAG_STRUCT_DATA_CONTIGUOUS: i32 = 0x80;
+
+/// Indicates the data stored in this pbuf can change. If this pbuf needs
+/// to be queued, it must be copied/duplicated.
+const PBUF_TYPE_FLAG_DATA_VOLATILE: i32 = 0x40;
+
+/// Indicates this pbuf is used for RX (if not set, indicates use for TX).
+/// This information can be used to keep some spare RX buffers e.g. for
+/// receiving TCP ACKs to unblock a connection)
+const PBUF_ALLOC_FLAG_RX: i32 = 0x0100;
+
+/// Indicates the application needs the pbuf payload to be in one piece
+const PBUF_ALLOC_FLAG_DATA_CONTIGUOUS: i32 = 0x0200;
+
+const PBUF_TYPE_ALLOC_SRC_MASK_STD_HEAP: i32 = 0x00;
+
+const PBUF_TYPE_ALLOC_SRC_MASK_STD_MEMP_PBUF: i32 = 0x01;
+
+const PBUF_TYPE_ALLOC_SRC_MASK_STD_MEMP_PBUF_POOL: i32 = 0x02;
+
+/// Enumeration of pbuf types
 #[repr(i32)]
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum pbuf_type {
-    PBUF_RAM = 0,
-    PBUF_ROM = 1,
-    PBUF_REF = 2,
-    PBUF_POOL = 3,
+    /// pbuf data is stored in RAM, used for TX mostly, struct pbuf and its payload
+    /// are allocated in one piece of contiguous memory (so the first payload byte
+    /// can be calculated from struct pbuf).
+    /// pbuf_alloc() allocates PBUF_RAM pbufs as unchained pbufs (although that might
+    /// change in future versions).
+    /// This should be used for all OUTGOING packets (TX).
+    PBUF_RAM = (PBUF_ALLOC_FLAG_DATA_CONTIGUOUS | PBUF_TYPE_FLAG_STRUCT_DATA_CONTIGUOUS | PBUF_TYPE_ALLOC_SRC_MASK_STD_HEAP),
+
+    /// pbuf data is stored in ROM, i.e. struct pbuf and its payload are located in
+    /// totally different memory areas. Since it points to ROM, payload does not
+    /// have to be copied when queued for transmission.
+    PBUF_ROM = PBUF_TYPE_ALLOC_SRC_MASK_STD_MEMP_PBUF,
+
+    /// pbuf comes from the pbuf pool. Much like PBUF_ROM but payload might change
+    /// so it has to be duplicated when queued before transmitting, depending on
+    /// who has a 'ref' to it.
+    PBUF_REF = (PBUF_TYPE_FLAG_DATA_VOLATILE | PBUF_TYPE_ALLOC_SRC_MASK_STD_MEMP_PBUF),
+
+    /// pbuf payload refers to RAM. This one comes from a pool and should be used
+    /// for RX. Payload can be chained (scatter-gather RX) but like PBUF_RAM, struct
+    /// pbuf and its payload are allocated in one piece of contiguous memory (so
+    /// the first payload byte can be calculated from struct pbuf).
+    /// Don't use this for TX, if the pool becomes empty e.g. because of TCP queuing,
+    /// you are unable to receive TCP acks! */
+    PBUF_POOL = (PBUF_ALLOC_FLAG_RX | PBUF_TYPE_FLAG_STRUCT_DATA_CONTIGUOUS | PBUF_TYPE_ALLOC_SRC_MASK_STD_MEMP_PBUF_POOL)
 }
 
+const PBUF_TRANSPORT_HLEN: i32 = 20;
+
+const PBUF_IP_HLEN: i32 = 40;
+
+/// PBUF_LINK_ENCAPSULATION_HLEN: the number of bytes that should be allocated
+/// for an additional encapsulation header before ethernet headers (e.g. 802.11)
+const PBUF_LINK_ENCAPSULATION_HLEN: i32 = 0;
+
+/// PBUF_LINK_HLEN: the number of bytes that should be allocated for a
+/// link level header. The default is 14, the standard value for Ethernet.
+const PBUF_LINK_HLEN: i32 = 14;
+
+
+/// Enumeration of pbuf layers
 #[repr(i32)]
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum pbuf_layer {
-    PBUF_TRANSPORT = 0,
-    PBUF_IP = 1,
-    PBUF_LINK = 2,
-    PBUF_RAW_TX = 3,
-    PBUF_RAW = 4,
+    /// Includes spare room for transport layer header, e.g. UDP header.
+    /// Use this if you intend to pass the pbuf to functions like udp_send().
+    PBUF_TRANSPORT = PBUF_LINK_ENCAPSULATION_HLEN + PBUF_LINK_HLEN + PBUF_IP_HLEN + PBUF_TRANSPORT_HLEN,
+
+    /// Includes spare room for IP header.
+    /// Use this if you intend to pass the pbuf to functions like raw_send().
+    PBUF_IP = PBUF_LINK_ENCAPSULATION_HLEN + PBUF_LINK_HLEN + PBUF_IP_HLEN,
+
+    /// Includes spare room for link layer header (ethernet header).
+    /// Use this if you intend to pass the pbuf to functions like ethernet_output().
+    PBUF_LINK = PBUF_LINK_ENCAPSULATION_HLEN + PBUF_LINK_HLEN,
+
+    /// Includes spare room for additional encapsulation header before ethernet
+    /// headers (e.g. 802.11).
+    /// Use this if you intend to pass the pbuf to functions like netif->linkoutput().
+    PBUF_RAW_TX = PBUF_LINK_ENCAPSULATION_HLEN,
+
+    // Use this for input packets in a netif driver when calling netif->input()
+    // in the most common case - ethernet-layer netif driver.
+    // Rust doesn't allow enums with the same value. Disabled, not needed anyway.
+    // PBUF_RAW = 0
 }
 
 #[repr(C)]
@@ -33,7 +110,8 @@ pub struct pbuf {
     pub len: u16,
     pub pbuf_type: u8,
     pub flags: u8,
-    pub pbuf_ref: u16,
+    pub pbuf_ref: u8,
+    pub if_idx: u8,
 }
 
 pub trait Pbuf {
